1c1c40f8fb634f54d75e633287cac4bb23b6596f
Paulina shao <PaulinaJshao@gmail.com>
2025-10-08 01:19:37 +0000

    Try fixing this error: `Runtime Error: Error: The default export is not

--- src/app/profile/[username]/page.tsx
+++
@@ -1,4 +0,0 @@
-// This file is intentionally left blank. 
-// The profile page has been moved to src/app/(profile)/u/[username]/page.tsx
-// to adopt the new routing structure.
-// This placeholder can be removed once the old route is fully deprecated.
