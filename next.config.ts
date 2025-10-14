854f8cd70c4f736b0c41a28f22b84cf6a8cb51e3
Paulina shao <PaulinaJshao@gmail.com>
2025-10-14 08:51:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig = {
         pathname: "/**",
       },
       {
-        protocol: "https://",
+        protocol: "https",
         hostname: "picsum.photos",
         pathname: "/**",
       },
