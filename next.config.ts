af858a8531bf5b90566514d35461be1f021dd477
Paulina shao <PaulinaJshao@gmail.com>
2025-10-14 01:20:18 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,7 @@ const nextConfig = {
     "http://localhost:3000",
     "http://10.88.0.3:3000",
     "*.cloudworkstations.dev",
+    "6000-firebase-akilipesacustomize-1759689616117.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev",
   ],
 };
 
