42a08cc448d9df31f97556f60824fab70e5aaac9
Paulina shao <PaulinaJshao@gmail.com>
2025-10-15 23:52:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -34,10 +34,6 @@ const nextConfig: NextConfig = {
     '*.cloudworkstations.dev',
     '6000-firebase-akilipesacustomize-1759689616117.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev',
   ],
-  experimental: {
-    workerThreads: false,
-    manualClientBasePath: true,
-  },
 };
 
 export default nextConfig;
