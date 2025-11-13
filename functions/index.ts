25bdc4effd78229adbd0bf19d83d34156bf8fd06
Paulina shao <PaulinaJshao@gmail.com>
2025-11-13 22:59:58 +0000

    Try fixing this error: `Server Error: TypeError: Cannot read properties

--- functions/index.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is deprecated and its contents have been merged into /src/index.ts.
-// It is kept for historical reference but is no longer the entry point for deployment.
-// To modify Cloud Functions, edit functions/src/index.ts.
