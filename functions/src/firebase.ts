3c50466b23c321f1ed472817879dca025e327c84
Paulina shao <PaulinaJshao@gmail.com>
2025-11-22 01:40:15 +0000

    please read careful and implement in detailed as per instruction:  here

---
+++ functions/src/firebase.ts
@@ -0,0 +1,12 @@
+// functions/src/firebase.ts
+import * as admin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+export const db = getFirestore();
+export const storage = getStorage();
+export { admin };
