1963166e5bc77a8709f45ea1e798d374a238b376
Paulina shao <PaulinaJshao@gmail.com>
2025-11-22 05:17:42 +0000

    please scan again as i have run the firebase deploy and the same error o

--- functions/lib/firebase/index.js
+++
@@ -1,8 +0,0 @@
-// functions/src/firebase/index.ts
-import * as admin from "firebase-admin";
-if (!admin.apps.length) {
-    admin.initializeApp();
-}
-export const db = admin.firestore();
-export const storage = admin.storage();
-export { admin };
